ba41c062ebc8538ba68979bf834a83ed0432ad07
zkq1314 <1529704655@qq.com>
2018-04-21 11:44:10 +0800

    注释语句

---
+++ 10_save_restore_net.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+import os
+
+# This shows how to save/restore your model (trained variables).
+# To see how it works, please stop this program during training and resart.
+# This network is the same as 3_net.py
+
+# 定义权重函数
+def init_weights(shape):
+    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
+# 定义模型
+def model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden): # this network is the same as the previous one except with an extra hidden layer + dropout
+	# 第一个全连接层
+    X = tf.nn.dropout(X, p_keep_input)
+    h = tf.nn.relu(tf.matmul(X, w_h))
+
+    h = tf.nn.dropout(h, p_keep_hidden)
+	# 第二个全连接层
+    h2 = tf.nn.relu(tf.matmul(h, w_h2))
+
+    h2 = tf.nn.dropout(h2, p_keep_hidden)
+	#输出预测值
+    return tf.matmul(h2, w_o)
+
+# 加载数据
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+
+X = tf.placeholder("float", [None, 784])
+Y = tf.placeholder("float", [None, 10])
+# 初始化权重
+w_h = init_weights([784, 625])
+w_h2 = init_weights([625, 625])
+w_o = init_weights([625, 10])
+# 生成模型，得到预测值
+p_keep_input = tf.placeholder("float")
+p_keep_hidden = tf.placeholder("float")
+py_x = model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden)
+# 定义损失函数
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
+train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+predict_op = tf.argmax(py_x, 1)
+
+# 存储路径
+ckpt_dir = "./ckpt_dir"
+if not os.path.exists(ckpt_dir):
+    os.makedirs(ckpt_dir)
+# 计数器变量，为训练轮数计数，trainable=FALSE不需要被训练
+global_step = tf.Variable(0, name='global_step', trainable=False)
+
+# Call this after declaring all tf.Variables.
+saver = tf.train.Saver()
+# 位于tf.train.Saver()后的变量不会被存储
+# This variable won't be stored, since it is declared after tf.train.Saver()
+non_storable_variable = tf.Variable(777)
+
+# Launch the graph in a session
+with tf.Session() as sess:
+    # you need to initialize all variables
+    tf.global_variables_initializer().run()
+
+    ckpt = tf.train.get_checkpoint_state(ckpt_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+        print(ckpt.model_checkpoint_path)
+        saver.restore(sess, ckpt.model_checkpoint_path) # restore all variables
+
+    start = global_step.eval() # 得到 global_step初始值
+    print("Start from:", start)
+
+    for i in range(start, 100):
+		# 以128为batch_size
+        for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
+            sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end],
+                                          p_keep_input: 0.8, p_keep_hidden: 0.5})
+
+        global_step.assign(i).eval() # 更新计数器
+        saver.save(sess, ckpt_dir + "/model.ckpt", global_step=global_step)
+        print(i, np.mean(np.argmax(teY, axis=1) ==
+                         sess.run(predict_op, feed_dict={X: teX, 
+                                                         p_keep_input: 1.0,
+                                                         p_keep_hidden: 1.0})))
